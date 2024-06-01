865ebbfbdd1506e261e800ec782646a70aee4f97
Basel Hussein <cv98@windowslive.com>
2024-06-01 14:03:32 +0000

    Changing secret key

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x89h2k++w6myw(68i4v3z*j01m9tpmgk*ef3-&^+)r-+@r#ze1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
