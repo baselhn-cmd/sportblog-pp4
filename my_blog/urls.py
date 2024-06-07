ebfea3afa70270b94b7303ab519ccb63384acad8
Basel Hussein <cv98@windowslive.com>
2024-06-07 20:32:05 +0000

    include urls

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from sportblog import views as index_views
+# from sportblog import views as index_views
 
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    # path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
-     path("", include("sportblog.urls")),
+    path("", include("sportblog.urls")),
 ]
