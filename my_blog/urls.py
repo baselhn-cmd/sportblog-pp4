cb19de767b0e371d09bb3fe59e927edee2000e8e
Basel Hussein <cv98@windowslive.com>
2024-06-08 07:01:41 +0000

    about

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from sportblog import views as index_views
+from sportblog import about as about
 
 
 urlpatterns = [
