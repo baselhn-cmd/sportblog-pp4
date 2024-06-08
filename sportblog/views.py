528b700acf59ac65769c2c7acf1619caa10811d8
Basel Hussein <cv98@windowslive.com>
2024-06-08 07:23:31 +0000

    add teamplate

--- sportblog/views.py
+++ sportblog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>My sport blog</h1>")
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse("Blog About")
+    return HttpResponse()
