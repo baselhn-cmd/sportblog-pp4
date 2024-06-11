e88228088c76223e966d48f6710328f3d60298bf
Basel Hussein <cv98@windowslive.com>
2024-06-11 18:22:08 +0000

    Add about ,home contact

--- sportblog/urls.py
+++ sportblog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    path('', views.home, name="sportblog-home"),
     path('about', views.about, name="blog-about"),
+    path('contact', views.contact, name="blog-contat"),
+    path('post', views.about, name="blog-post"),
 ]
--- sportblog/views.py
+++ sportblog/views.py
@@ -3,32 +3,16 @@ from django.http import HttpResponse
 from .models import Post
 from django.views import generic
 
-posts = [
-    {
-        'author': 'Destiny Franks',
-        'title': 'Blog Post1',
-        'content': 'This is my first blog post',
-        'date_posted': '7th August, 2021',
-    },
 
-{
-        'author': 'Jane Doe',
-        'title': 'Blog Post2',
-        'content': 'This is my second blog post',
-        'date_posted': '7th August, 2021',
-    }
-    
-]
 # Create your views here.
 def home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+    return render(request, 'blog/home.html')
 
 def about(request):
     return render(request, 'blog/about.html')
 
-# class PostList(generic.ListView):
-#     queryset = Post.objects.filter(status=1).oreder_by('-created_on')
-#     template_name = 'index.html'
+def contact(request):
+    return render(request, 'blog/contat.html')
+
+def post_details(request):
+    return render(request, 'blog/post_details.html')
