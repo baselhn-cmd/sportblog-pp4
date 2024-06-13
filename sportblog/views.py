6c8b76ba204be518baa3508900cc787eaeb58155
Basel Hussein <cv98@windowslive.com>
2024-06-13 17:04:02 +0000

    Changed database and fixed some error in codes

--- sportblog/views.py
+++ sportblog/views.py
@@ -7,9 +7,21 @@ from .models import Homepage
 
 # Create your views here.
 def home(request):
-    page= Homepage.objects.all()[0]
-    return render(request, 'sportblog/home.html'), {'page': page}
+    home_data = Homepage.objects.all()
 
+    if not home_data:
+        return render(request, 'sportblog/home.html', {'error': 'No homepage data found.'})
+
+    try:
+        first_item = home_data[0]
+    except IndexError:
+        first_item = None  # Handle the case where the list is empty
+
+    context = {
+        'first_item': first_item,
+    }
+    return render(request, 'sportblog/home.html', context)
+    
 def about(request):
     return render(request, 'sportblog/about.html')
 
