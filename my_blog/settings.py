595a6bc703535f0795fe996fa70567bbbdab49f1
Basel Hussein <cv98@windowslive.com>
2024-06-15 09:54:12 +0000

    changing database url in heroku and debug to false

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sportblog-pp4-e79d6c79885b.herokuapp.com',
                 '.herokuapp.com',
